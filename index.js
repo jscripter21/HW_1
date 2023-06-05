d9b2bf1dbaaf35decf757894ed6e964c5a85cd61
jscripter21 <jscripter21@gmail.com>
2023-06-05 17:45:11 +0300

    add hm 1

---
+++ index.js
@@ -0,0 +1,31 @@
+// one Home Work===
+
+// let name = prompt(message = 'What is your name ?');
+// let agePeople = prompt(message = 'How old are you ?');
+// let numberOne = prompt(message = 'Your favorite number ?');
+// let numberTwo = prompt(message = 'Think of any number?');
+// let result = +numberOne + +numberTwo;
+
+// alert('Hello, ' + name + ' You age :' + agePeople + ' You result is :' + result);
+
+
+// two Home Work===
+
+// let city = prompt(message = 'Я люблю місто ____.');
+// let data = prompt(message = 'Я там останній раз був в _____ році.');
+// let info = prompt(message = 'Також це місто відоме ______.'); 
+
+
+// alert('Я люблю місто ' + city + '. Я там останній раз був в ' + data + ' році.' + ' Також це місто відоме ' + info);
+
+
+// const cityName = prompt('Укажіть назву міста');
+// const cityData = prompt('Укажіть рік');
+// const cityInfo = prompt('Укажіть переваги міста');
+// city = 'Я люблю місто ' + cityName + '.';
+// data = 'Я там останній раз був в ' + cityData + 'р.';
+// info = 'Також це місто відомо ' + cityInfo + '.';
+
+// console.log(city + ' ' + data + ' ' + info);
+
+// alert('Я люблю місто ' + city + '. Я там останній раз був в ' + data + ' році.' + ' Також це місто відоме ' + info);
